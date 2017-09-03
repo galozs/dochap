3fa8bc04e67b0adea33ab626f755b7a813856692
Nitzanel <nitzanel2@gmail.com>
2017-09-03 11:49:17 +0300

    bash script calls correctly to ucsc_downloader.py

--- updater.sh
+++ updater.sh
@@ -2,12 +2,16 @@
 # move new read me to old readme
 mv db/readme_new db/readme_old
 # extract them
-echo "extracting the files..."
+echo "Extracting the files..."
 gunzip db/protein.gbk.gz -f
+echo "Updating ucsc tables..."
+rm db/transcript_aliases.txt -f
+rm db/transcript_data.txt -f
+python ucsc_downloader.py
 echo "Deleting old database..."
 rm db/aliases.db -f
 rm db/comb.db -f
 python db_creator.py
 #rm db/protein.gbk -f
-echo "setup complete."
+echo "Setup complete."
 
