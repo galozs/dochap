b030b4a14081e82f2be07727a45dc226c2a57853
Nitzanel <nitzanel2@gmail.com>
2017-09-04 18:46:56 +0300

    changes to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup)
+setup(
     name='dochap',
     version='1.0.0',
     author='Nitzan Elbaz',
     author_email='elbazni@post.bgu.ac.il',
-    packages = find_packages(),
+    packages = ['dochap'],
     url='https://github.com/nitzanel/dochap',
-    license='MIT License',
+    license='MIT',
     description='do some intresting things with your gtf files',
     long_description='some more info',
     zip_safe=False,
     install_requires=['pathos','dill']
-
+)
