c688801c02858e9c5c00255e632755a03868681c
Nitzanel <nitzanel2@gmail.com>
2017-09-20 10:45:24 +0300

    updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='dochap',
-    version='1.0.1',
+    version='1.0.3',
     author='Nitzan Elbaz',
     author_email='elbazni@post.bgu.ac.il',
     packages = ['dochap'],
